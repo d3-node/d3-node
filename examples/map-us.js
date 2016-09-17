4a57b69cc4fb59f2f56b9dccfc150d19a34efb9b
Brad Oyler <bradoyler@gmail.com>
2016-09-16 21:44:54 -0400

    modified map-us example

--- examples/map-us.js
+++ examples/map-us.js
@@ -1,18 +1,19 @@
 const topojson = require('topojson');
-const us = require('./data/us.json');
+const us = require('./data/us-states.json'); // source: https://github.com/bradoyler/atlas-make/tree/master/us-states
 const D3Node = require('./../index');
 const d3 = require('d3');
 
-// adapted from: https://bl.ocks.org/mbostock/6406992
+// adapted from: http://bl.ocks.org/bradoyler/bae6adf4e1e74c925ea20bf6026614b3
+
 var options = {svgStyles:'.mesh{fill: none;stroke: #333;stroke-width: .5px;stroke-linejoin: round;}'};
 
 var d3n = new D3Node(options);
 
 var width = 960,
   height = 500;
-
+var projection = d3.geo.albersUsa();
 var path = d3.geo.path()
-  .projection(null);
+  .projection(projection);
 
 var svg = d3n.createSVG()
   .attr("width", width)
@@ -21,7 +22,6 @@ var svg = d3n.createSVG()
   .datum(topojson.mesh(us))
   .attr("class", "mesh")
   .attr("d", path);
-
 
 // create output files
 require('./lib/output')('map-us', d3n);
