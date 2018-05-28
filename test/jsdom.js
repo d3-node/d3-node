afa0747aff6537b41d160f388656890de098a554
Fabio Farzat <fabiofarzat@gmail.com>
2018-05-28 14:36:23 -0300

    Adding testing

--- test/jsdom.js
+++ test/jsdom.js
@@ -5,8 +5,13 @@ const assert = require('assert')
 const D3Node = require('./../index')
 
 describe('jsDom features', function () {
-  var d3nA = new D3Node({container: '<!DOCTYPE html>test A'})
-  var d3nB = new D3Node({container: 'test B'})
+  var d3nA = new D3Node({ container: '<!DOCTYPE html>test A' })
+  var d3nB = new D3Node({ container: 'test B' })
+  var d3nC = new D3Node()
+
+  it('Test C: should serializeDocument()', function () {
+    assert.equal(d3nC.html(), '<html><head></head><body></body></html>')
+  })
 
   it('Test B: should serializeDocument()', function () {
     assert.equal(d3nB.html(), '<html><head></head><body>test B</body></html>')
